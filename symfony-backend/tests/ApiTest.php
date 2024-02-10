6d015dd58f5ad84ca67f4f2fdc51adcca08a92c1
Robin Brackez <robin.brackez@gmail.com>
2024-02-10 14:19:47 +0100

    Add api call tests

---
+++ symfony-backend/tests/ApiTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+
+class ApiTest extends ApiTestCase
+{
+    public function testApiCalls(): void
+    {
+        $client = $this->createClient();
+        $statusCode = $client->request('GET', '/api/meeting-rooms')->getStatusCode();
+        $this->assertEquals(200, $statusCode);
+        $statusCode = $client->request('GET', '/api/bookings')->getStatusCode();
+        $this->assertEquals(200, $statusCode);
+        $statusCode = $client->request('POST', '/api/bookings', [
+            'headers' => [
+                'Content-Type' => 'application/ld+json',
+            ],
+            'json' => [
+                'startsAt' => '2025-01-04T03:30:00+00:00',
+                'endsAt' => '2025-01-04T04:30:00+00:00',
+                'meetingRoom' => '/api/meeting-rooms/1',
+                'bookedBy' => '/api/users/1',
+            ],
+        ])->getStatusCode();
+        $this->assertEquals(201, $statusCode);
+    }
+}
