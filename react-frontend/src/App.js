88bd66fb68407b1a96d1e5ca871d525570ff4f84
Robin Brackez <robin.brackez@gmail.com>
2024-02-11 10:00:26 +0100

    Add datepickers to form

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {connect} from "react-redux";
 import {fetchMeetingRooms} from "./redux/actions/meetingRoomActions";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import MeetingRoom from "./components/MeetingRoom";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -19,10 +21,38 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 }
 
 const App = (props) => {
+  const initialStartTime = new Date();
+  initialStartTime.setHours(8);
+  const initialEndTime = new Date();
+  initialEndTime.setHours(17);
+  const [booking, setBooking] = useState({
+    isValid: false,
+    selectedDate: null,
+    startTime: initialStartTime,
+    endTime: initialEndTime,
+    meetingRoom: null,
+    email: ''
+  });
 
   useEffect(() => {
     props.fetchMeetingRoomsAction();
   }, []);
+
+  const minDate = new Date();
+  const maxDate = new Date();
+  maxDate.setDate(maxDate.getDate() + 7);
+
+  function onStartDateChanged(date) {
+    setBooking({...booking, selectedDate: date});
+  }
+
+  function onEndTimeChanged(time) {
+    setBooking({...booking, endTime: time});
+  }
+
+  function onStartTimeChanged(time) {
+    setBooking({...booking, startTime: time});
+  }
 
   return (
     <div className="App">
@@ -40,28 +70,60 @@ const App = (props) => {
                 <form>
                   <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input type="time" className="form-control" id="email"/>
+                    <input type="email" className="form-control" id="email"/>
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="startTime" className="form-label">Start Time</label>
-                    <input type="time" className="form-control" id="startTime"/>
+                    <label htmlFor="startDate" className="form-label">Date</label>
+                    <DatePicker
+                      name="startDate"
+                      selected={booking.selectedDate}
+                      onChange={(date) => onStartDateChanged(date)}
+                      minDate={minDate}
+                      maxDate={new Date(maxDate)}
+                      dateFormat="yyyy/MM/dd"
+                    />
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="endTime" className="form-label">End Time</label>
-                    <input type="time" className="form-control" id="endTime"/>
+
+                    <div>
+                      <p>Select Start Time:</p>
+                      <DatePicker
+                        selected={booking.startTime}
+                        onChange={time => onStartTimeChanged(time)}
+                        showTimeSelect
+                        showTimeSelectOnly
+                        timeIntervals={15} // Time selection intervals in minutes
+                        timeCaption="Start Time"
+                        dateFormat="h:mm aa" // Format for time display
+                      />
+                    </div>
+                    <div>
+                      <p>Select End Time:</p>
+                      <DatePicker
+                        selected={booking.endTime}
+                        onChange={time => onEndTimeChanged(time)}
+                        showTimeSelect
+                        showTimeSelectOnly
+                        timeIntervals={15} // Time selection intervals in minutes
+                        timeCaption="End Time"
+                        dateFormat="h:mm aa" // Format for time display
+                        minTime={booking.startTime} // Ensuring end time is after start time
+                        maxTime={new Date(booking.startTime).setHours(23, 45)} // Latest selectable time
+                      />
+                    </div>
                   </div>
                   <div className="container text-center">
                     <div className="row row-cols-2 g-lg-3">
                       {props.meetingRooms.length > 0 &&
                         props.meetingRooms.map((room, index) => (
                           <div className="col" key={index}>
-                            <MeetingRoom {...room} isAvailable />
+                            <MeetingRoom {...room} isAvailable={null} statusMessage={null} />
                           </div>
                         ))
                       }
                     </div>
                   </div>
-                  <button type="submit" className="btn btn-primary">Book Now</button>
+                  <button type="submit" className="btn btn-primary" disabled={!booking.isValid}>Book Now</button>
                 </form>
               </div>
             </div>
--- react-frontend/src/components/MeetingRoom/MeetingRoom.js
+++ react-frontend/src/components/MeetingRoom/MeetingRoom.js
@@ -1,11 +1,14 @@
 
 const MeetingRoom = (props) => {
+
+  const backgroundColor = props.isAvailable === null ? 'light' : props.isAvailable ? 'success' : 'danger';
+
   return (
-    <div className="card">
+    <div className={"card text-bg-" + backgroundColor}>
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
-        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <button className="btn btn-primary" onClick={() => props.onSelect()} disabled={!props.isAvailable}>Select</button>
+        <p className="card-text">{props.statusMessage}</p>
+        <button className={"btn btn-" + backgroundColor} onClick={() => props.onSelect()} disabled={!props.isAvailable}>Select</button>
       </div>
     </div>
 
