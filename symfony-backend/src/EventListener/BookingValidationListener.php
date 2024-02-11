cb4c25592788cb321d65a35abb98514f8dd6d75c
Robin Brackez <robin.brackez@gmail.com>
2024-02-11 10:05:58 +0100

    Add extra validation to the BookingValidationListener

--- symfony-backend/src/EventListener/BookingValidationListener.php
+++ symfony-backend/src/EventListener/BookingValidationListener.php
@@ -26,11 +26,14 @@ class BookingValidationListener
         if ($booking->getStartsAt() < $this->allowedStartDate || $booking->getEndsAt() > $this->allowedEndDate) {
             throw new OutOfRangeBookingException(sprintf('Booking outside of allowed time range. Allowed between: %s and %s', $this->allowedStartDate->format('Y-m-d H:i:s'), $this->allowedEndDate->format('Y-m-d H:i:s')));
         }
+        if ($booking->getStartsAt()->format('Y-m-d') !== $booking->getEndsAt()->format('Y-m-d')) {
+            throw new OutOfRangeBookingException('Booking can\'t take place over more than one day');
+        }
         if ($booking->getStartsAt() > $booking->getEndsAt()) {
             throw new \InvalidArgumentException('Start date cannot be after end date');
         }
         if ($booking->getStartsAt() === $booking->getEndsAt()) {
-            throw new \InvalidArgumentException('Start date cannot be equal to the end date');
+            throw new \InvalidArgumentException('Start time cannot be equal to the end time');
         }
         $bookingRepository = $args->getObjectManager()->getRepository(Booking::class);
         $overlappingBooking = $bookingRepository->getOverlappingBooking($booking);
